2dbe2087ebf7731ece080e18585081e11320e818
kenta0313 <asanokenta@me.com>
2021-12-14 15:25:11 +0900

    ✨ create redux

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useDispatch, useSelector } from 'react-redux'
+import Sample from '../components/Sample'
+import { LogState } from '../features/log'
+import { AppState } from '../store'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const { count, log } = useSelector<
+    AppState,
+    { count: number; log: LogState }
+  >((state) => ({
+    count: state.counter.count,
+    log: state.log,
+  }));
   return (
     <div className={styles.container}>
       <Head>
@@ -13,44 +24,10 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <div style={{ padding: '12px', width: '400px', backgroundColor: '#ccc' }}>
+        <h1>App{count}</h1>
+        <Sample samplePropData="Data passed from parent." />
+      </div>
       </main>
 
       <footer className={styles.footer}>
