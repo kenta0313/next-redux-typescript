dd726ca1b9b8db9fa62563970f0a7afd2b7940f8
kenta0313 <asanokenta@me.com>
2021-12-14 16:49:33 +0900

    ✏️

--- src/selector/selector.ts
+++ src/selector/selector.ts
@@ -1,4 +1,6 @@
-import { RootState } from "../store/store";
+import { store } from "../store/store";
+
+export type RootState = ReturnType<typeof store.getState>;
 
 export const countSelector = (state: RootState) => state.counter.count;
 
--- src/store/store.ts
+++ src/store/store.ts
@@ -17,5 +17,3 @@ const rootReducer = combineReducers<AppState>({
 export const store = configureStore({
   reducer: rootReducer,
 });
-
-export type RootState = ReturnType<typeof store.getState>;
